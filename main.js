dd140b764fb3b9d501c20b3f0a8977c7d63b2572
odaka <noppiki@gmail.com>
2025-11-22 18:19:40 +0900

    Initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+  const observerOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.1
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+        observer.unobserve(entry.target);
+      }
+    });
+  }, observerOptions);
+
+  const hiddenElements = document.querySelectorAll('.hidden-left, .hidden-right, .hidden-up');
+  hiddenElements.forEach((el) => observer.observe(el));
+});
